4d907543e8f00a54c9abc6837ef5bacba3075119
Koudai Yagisawa <yagisawakodai@gmail.com>
2019-07-27 15:48:48 +0900

    nginxの設定

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ENV["RAILS_RELATIVE_URL_ROOT"] = '/demo'
+Rails.application.config.relative_url_root = '/demo'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :books do
-    get 'show_subjects'
+  scope (Rails.application.config.relative_url_root || '/') do
+    get '/', to: 'books#index'
+    resources :books do
+      get 'show_subjects'
+    end
   end
 end
